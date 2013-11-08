478696aa3511b730574ffee1258fba8b65d82611
Joel Kåberg <joel.kaberg@gmail.com>
2013-11-08 15:33:01 +0100

    match current ip in config file

--- vpnmon.py
+++ vpnmon.py
@@ -3,6 +3,11 @@ import socket
 import fcntl
 import struct
 import os
+import re
+
+### Requirements!
+# First line in .rtorrent.rc needs to be the bind value, for example: bind = 192.168.1.1
+# Python 2.6
 
 ### Settings
 ip_range = "178.132"
@@ -15,6 +20,12 @@ def intf_addr(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
 
+def read_addr(cfg_file):
+    f = open(cfg_file)
+    first_line = f.readline()
+    f.close()
+    return ''.join(re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}", first_line))
+
 # Writes the new IP address to the config file
 def write_addr(cfg_file, bind_addr):
     f = open(cfg_file)
@@ -23,11 +34,11 @@ def write_addr(cfg_file, bind_addr):
     t.write(bind_addr + "\n")
     t.write(the_rest)
     t.close()
+    return
 
 def main():
     ip_addr = intf_addr(intf)
-
-    if not ip_addr.startswith(ip_range):
+    if not ip_addr.startswith(ip_range) or read_addr(settings_file) != ip_addr:
         write_addr(settings_file, "bind = %s" % ip_addr)
         os.system(exec_cmd)
 
